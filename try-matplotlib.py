331437756a8f6582179c308589cba05b2564b2e4
toshinori.sato <tyahhahha@gmail.com>
2025-09-06 23:55:55 +0900

    try matplotlib 2

--- try-matplotlib.py
+++ try-matplotlib.py
@@ -2,6 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.arange(0, 6, 0.1)
-y = np.sin(x)
-plt.plot(x, y)
+y1 = np.sin(x)
+y2 = np.cos(x)
+
+plt.plot(x, y1, label="sin")
+plt.plot(x, y2, label="cos", linestyle="--")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title(" sin & cos")
+plt.legend()
 plt.show()
