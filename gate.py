4f769a5c2fe4fcb06bcb19326ef6959e5fd50856
toshinori.sato <tyahhahha@gmail.com>
2025-09-07 22:38:17 +0900

    AND impl via gateConstructor

--- gate.py
+++ gate.py
@@ -1,13 +1,15 @@
 # impl AND
 import numpy as np
 
-def AND(x1, x2):
-  w1, w2, b = 0.5, 0.5, -0.7
-  x = np.array([x1,x2])
-  y = np.array([w1,w2])
-  tmp = np.sum(x * y) + b
-  if tmp <= 0:
-    return 0
-  else:
-    return 1
-  
+def gateConstructor(w1,w2,b):
+  def gate(x1,x2):
+    x = np.array([x1,x2])
+    y = np.array([w1,w2])
+    tmp = np.sum(x * y) + b
+    if tmp <= 0:
+      return 0
+    else:
+      return 1
+  return gate
+
+AND = gateConstructor(0.5, 0.5, -0.7)
