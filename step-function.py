c85d8b1512132f0761d366dcec8d4efa4444f60e
toshinori.sato <tyahhahha@gmail.com>
2025-09-08 22:57:05 +0900

    plot step function

---
+++ step-function.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+def step_function(x):
+  y = x > 0
+  return y.astype(int)
+
+def main():
+  x = np.arange(-5.0,5.0,0.1)
+  y = step_function(x)
+  plt.plot(x, y)
+  plt.ylim(-0.1,1.1)
+  plt.show()
+
+if __name__ == "__main__":
+  main()
