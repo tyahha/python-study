064a1a6a176006777737cdcf831e1a41f6fa4661
toshinori.sato <tyahhahha@gmail.com>
2025-09-21 15:00:04 +0900

    solve b06

--- competitive/main.py
+++ competitive/main.py
@@ -1,15 +1,28 @@
-_, q = map(int, input().split(" "))
+input()
 
 aa = list(map(int, input().split(" ")))
-sums = []
-sum = 0
+
+q = int(input())
+
+sums = [[0,0]]
+wins = 0
+looses = 0
 for a in aa:
-  sum += a
-  sums.append(sum)
+  if a == 1:
+    wins += 1
+  else:
+    looses += 1
+  sums.append([wins, looses])
 
 for i in range(q):
   l, r = map(int, input().split(" "))
-  if l == 1:
-    print(sums[r-1])
+  pw, pl = sums[r]
+  qw, ql = sums[l-1]
+  wins = pw - qw
+  looses = pl - ql
+  if wins > looses:
+    print("win")
+  elif wins < looses:
+    print("lose")
   else:
-    print(sums[r-1] - sums[l-2])
+    print("draw")
