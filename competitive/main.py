764ed6b2bef8ff5551f119ef4fa3ed442cece4d8
toshinori.sato <tyahhahha@gmail.com>
2025-09-24 22:27:39 +0900

    solve b07

--- competitive/main.py
+++ competitive/main.py
@@ -1,11 +1,11 @@
-d = int(input())
+t = int(input())
 n = int(input())
-aa = [0] * (d + 1)
-lrs = [list(map(int, input().split())) for _ in range(n)]
-for l, r in lrs:
-    aa[l - 1] += 1
+aa = [0] * (t + 1)
+for _ in range(n):
+    [l, r] = list(map(int, input().split()))
+    aa[l] += 1
     aa[r] -= 1
 count = 0;
-for i in range(1, d+1):
-    count += aa[i - 1]
+for i in range(t):
+    count += aa[i]
     print(count)
